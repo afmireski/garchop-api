ffcbe2646e89156f524ea0102fff3306d2737c90
afmireski <afmireski@outlook.com>
2024-03-19 20:40:07 -0300

    config: Setup chi router

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func main()  {
+	r := chi.NewRouter()
+
 	fmt.Println("API is running...")
+	http.ListenAndServe(":3000", r)
 }

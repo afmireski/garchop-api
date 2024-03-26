5c48f1df5b7e0c95aa06285fcff89928d350ce24
afmireski <afmireski@outlook.com>
2024-03-26 17:26:13 -0300

    #5 - feat: Implement SupabaseUsersRepoAdapter.Update

--- internal/adapters/supabase-users-repository.go
+++ internal/adapters/supabase-users-repository.go
@@ -1,7 +1,10 @@
 package adapters
 
 import (
+	"encoding/json"
 	"errors"
+
+	"github.com/afmireski/garchop-api/internal/models"
 	"github.com/afmireski/garchop-api/internal/ports"
 	supabase "github.com/nedpals/supabase-go"
 )
@@ -14,6 +17,20 @@ func NewSupabaseUsersRepository(client *supabase.Client) *SupabaseUsersRepositor
 	return &SupabaseUsersRepository{
 		client: client,
 	}
+}
+
+func serializeMany(data []map[string]string) ([]models.UserModel, error) {
+	jsonData, err := json.Marshal(data)
+
+	if err != nil {
+		return nil, err
+	}
+
+
+	var result []models.UserModel
+	json.Unmarshal(jsonData, &result)
+
+	return result, nil
 }
 
 func (r *SupabaseUsersRepository) Create(input ports.CreateUserInput) (string, error) {
@@ -36,8 +53,27 @@ func (r *SupabaseUsersRepository) FindById(id string) (interface{}, error) {
 	return nil, errors.New("not implemented")
 }
 
-func (r *SupabaseUsersRepository) Update(id string, input interface{}) (interface{}, error) {
-	return nil, errors.New("not implemented")
+func (r *SupabaseUsersRepository) Update(id string, input interface{}, where map[string]map[string]string) (interface{}, error) {
+	var supabaseData []map[string]string
+
+	query := r.client.DB.From("users").Update(input).Eq("id", id)
+	if len(where) > 0 {
+		for column, filter := range where {
+			for operator, criteria := range filter {
+				query = query.Filter(column, operator, criteria)
+			}
+		}
+	}
+
+	err := query.Execute(&supabaseData); if err != nil {
+		return nil, err
+	}
+
+	result, err := serializeMany(supabaseData); if err != nil {
+		return nil, err
+	}	
+
+	return result[0], nil
 }
 
 func (r *SupabaseUsersRepository) Delete(id string) error {

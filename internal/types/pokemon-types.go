6b06a0a53af58ceea59023d8e37cb38fdc17ba75
afmireski <afmireski@outlook.com>
2024-04-04 14:35:09 -0300

    #9 - feat: Define NewPokemonInput type

--- internal/types/pokemon-types.go
+++ internal/types/pokemon-types.go
@@ -1,11 +1,18 @@
 package types
 
 type CreatePokemonInput struct {
-	ReferenceId int `json:"reference_id"`
-	TierId string `json:"tier_id"`
-	Name string `json:"name"`
-	Weight int `json:"weight"`
-	Height int `json:"height"`
-	ImageUrl string `json:"image_url"`
-	Experience int `json:"experience"`
+	ReferenceId int    `json:"reference_id"`
+	TierId      string `json:"tier_id"`
+	Name        string `json:"name"`
+	Weight      int    `json:"weight"`
+	Height      int    `json:"height"`
+	ImageUrl    string `json:"image_url"`
+	Experience  int    `json:"experience"`
+}
+
+type NewPokemonInput struct {
+	Name         string `json:"name"`
+	Price        int    `json:"price"`
+	InitialStock int    `json:"stock"`
+	TierId       string `json:"tier_id"`
 }

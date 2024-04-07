46922b56971e9aea65c7defde7c6ca3b259c728d
Alexandre Tolomeotti <alexandretolomeotti@alunos.utfpr.edu.br>
2024-04-07 13:45:42 -0300

    fix: Remove unused UsersUpdateInput dto
    Duplicate of /internal/types/input-types.go/NewUserInput

--- internal/ports/user-repository-port.go
+++ internal/ports/user-repository-port.go
@@ -23,12 +23,3 @@ type CreateUserInput struct {
 	Phone     string    `json:"phone"`
 	BirthDate time.Time `json:"birth_date"`
 }
-
-type UpdateUserInput struct {
-	Name          string    `json:"name"`
-	Email         string    `json:"email"`
-	Phone         string    `json:"phone"`
-	Password      string    `json:"password"`
-	PlainPassword string    `json:"plain_password"`
-	BirthDate     time.Time `json:"birth_date"`
-}

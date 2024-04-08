eb448215e0e03f4340a70ae4f3a8d15d7edad7ce
afmireski <afmireski@outlook.com>
2024-04-08 20:54:19 -0300

    #20 - feat: Define PokemonModel

---
+++ internal/models/pokemon-model.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type PokemonModel struct {
+	id string `json:"id"`
+	ReferenceId uint `json:"reference_id"`
+	TierId string `json:"tier_id"`
+	Name string `json:"name"`
+	Weight int `json:"weight"`
+	Height int `json:"height"`
+	ImageUrl string `json:"image_url"`
+	Experience int `json:"experience"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+}

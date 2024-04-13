eb39be82e218a34e1989d9a5119e3e23611a913c
afmireski <afmireski@outlook.com>
2024-04-13 11:10:00 -0300

    #27 - feat: Implement Tier FindById handler in TierController

--- internal/web/controllers/tiers-controller.go
+++ internal/web/controllers/tiers-controller.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/afmireski/garchop-api/internal/services"
+	"github.com/go-chi/chi/v5"
 )
 
 type TiersController struct {
@@ -26,6 +27,21 @@ func (c *TiersController) FindAll(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
+func (c *TiersController) FindById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	idParam := chi.URLParam(r, "id")
+
+	response, err := c.service.FindById(idParam); if err != nil {
+		w.WriteHeader(err.HttpCode)
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+		
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(response)
 }

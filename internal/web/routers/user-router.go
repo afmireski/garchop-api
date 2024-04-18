7c076f9e8725bc21e7f3c4f80514d3aad35d4747
Alexandre Tolomeotti <alexandretolomeotti@alunos.utfpr.edu.br>
2024-04-17 22:16:31 -0300

    feat(users): Add ``get`` route to retrieve admins

--- internal/web/routers/user-router.go
+++ internal/web/routers/user-router.go
@@ -9,6 +9,7 @@ func SetupUsersRouter(router *chi.Mux, controller *controllers.UsersController) 
 	router.Post("/users/new", controller.NewClient)
 	router.Patch("/users/{id}/update", controller.UpdateClient)
 	router.Get("/users/{id}", controller.GetUserById)
+	router.Get("/users/admin", controller.GetAdmins)
 	router.Delete("/users/{id}/del", controller.DeleteClientAccount)
 
 	router.Post("/admin/new", controller.NewAdministrator)

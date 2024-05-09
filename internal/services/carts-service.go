22b7c17952199f7e7f387702cbf52a32108c0798
afmireski <afmireski@outlook.com>
2024-05-08 21:05:20 -0300

    #38 - feat: Implement AddItemToCart method in CartsService

--- internal/services/carts-service.go
+++ internal/services/carts-service.go
@@ -14,12 +14,16 @@ import (
 type CartsService struct {
 	cartsRepository ports.CartsRepositoryPort
 	itemsRepository ports.ItemsRepositoryPort
+	priceRepository ports.PriceRepositoryPort
+	stockRepository ports.StockRepositoryPort
 }
 
-func NewCartsService(cartsRepository ports.CartsRepositoryPort, itemsRepository ports.ItemsRepositoryPort) *CartsService {
+func NewCartsService(cartsRepository ports.CartsRepositoryPort, itemsRepository ports.ItemsRepositoryPort, priceRepository ports.PriceRepositoryPort, stockRepository ports.StockRepositoryPort) *CartsService {
 	return &CartsService{
 		cartsRepository: cartsRepository,
 		itemsRepository: itemsRepository,
+		priceRepository: priceRepository,
+		stockRepository: stockRepository,
 	}
 }
 
@@ -28,17 +32,19 @@ func (s *CartsService) GetCurrentUserCart(user_id string) (*entities.Cart, *cust
 		return nil, customErrors.NewInternalError("invalid user_id", 400, []string{"the user_id must be a valid uuid"})
 	}
 
-	cartsRepositoryData, crErr := s.cartsRepository.FindLastCart(user_id); if crErr != nil {
+	cartsRepositoryData, crErr := s.cartsRepository.FindLastCart(user_id)
+	if crErr != nil {
 		return nil, customErrors.NewInternalError("failed on get the cart", 500, []string{crErr.Error()})
 	} else if cartsRepositoryData == nil {
 		return nil, customErrors.NewInternalError("cart not found", 404, []string{})
 	}
 
 	irWhere := myTypes.Where{
-		"cart_id": map[string]string{"eq": cartsRepositoryData.Id},
+		"cart_id":    map[string]string{"eq": cartsRepositoryData.Id},
 		"deleted_at": map[string]string{"is": "null"},
 	}
-	itensRepositoryData, irErr := s.itemsRepository.FindAll(irWhere); if irErr != nil {
+	itensRepositoryData, irErr := s.itemsRepository.FindAll(irWhere)
+	if irErr != nil {
 		return nil, customErrors.NewInternalError("failed on get the cart", 500, []string{irErr.Error()})
 	}
 
@@ -58,7 +64,7 @@ func validateAddItemToCartInput(input myTypes.AddItemToCartInput) *customErrors.
 		return customErrors.NewInternalError("invalid quantity", 400, []string{"the quantity must be greater than 0"})
 	}
 
-	return nil;
+	return nil
 }
 
 func (s *CartsService) AddItemToCart(userId string, input myTypes.AddItemToCartInput) *customErrors.InternalError {
@@ -67,22 +73,61 @@ func (s *CartsService) AddItemToCart(userId string, input myTypes.AddItemToCartI
 		return inputErr
 	}
 
-	cart, findCartErr := s.cartsRepository.FindLastCart(userId); if findCartErr != nil {
+	// Find Pokemon Price
+	price, priceErr := s.priceRepository.FindCurrentPrice(input.PokemonId)
+	if priceErr != nil {
+		return customErrors.NewInternalError("failed on get pokemon infos", 500, []string{priceErr.Error()})
+	}
+
+	if price.Pokemons.Stock.Quantity < input.Quantity || price.Pokemons.Stock.Quantity == 0 {
+		return customErrors.NewInternalError("this pokemon is out of stock", 400, []string{priceErr.Error()})
+	}
+
+	// Get the user cart
+	cart, findCartErr := s.cartsRepository.FindLastCart(userId)
+	if findCartErr != nil {
 		return customErrors.NewInternalError("failed on get the cart", 500, []string{findCartErr.Error()})
 	} else if cart == nil {
 		newCartInput := myTypes.CreateCartInput{
-			UserId: userId,
-			IsActive: true,
+			UserId:    userId,
+			IsActive:  true,
 			ExpiresIn: time.Now().Add(time.Hour * 1),
 		}
-		newCart, createCartErr := s.cartsRepository.Create(newCartInput); if createCartErr != nil {
+		newCart, createCartErr := s.cartsRepository.Create(newCartInput)
+		if createCartErr != nil {
 			return customErrors.NewInternalError("failed on get the cart", 500, []string{findCartErr.Error()})
 		}
 
 		cart = newCart
 	}
 
-	
+	itemInput := myTypes.CreateItemInput{
+		CartId:    cart.Id,
+		PokemonId: input.PokemonId,
+		Price:     price.Value,
+		Quantity:  input.Quantity,
+		Total:     price.Value * input.Quantity,
+	}
 
-	return nil	
+	s.itemsRepository.Create(itemInput)
+
+	cartWhere := myTypes.Where{
+		"id":         map[string]string{"eq": cart.Id},
+		"deleted_at": map[string]string{"is": "null"},
+	}
+
+	cartData := myTypes.AnyMap{
+		"total": cart.Total + price.Value*input.Quantity,
+	}
+
+	_, updateCartErr := s.cartsRepository.Update(cart.Id, cartData, cartWhere)
+	if updateCartErr != nil {
+		return customErrors.NewInternalError("failed on update cart total", 500, []string{updateCartErr.Error()})
+	}
+
+	s.stockRepository.Update(input.PokemonId, myTypes.AnyMap{"quantity": price.Pokemons.Stock.Quantity - input.Quantity}, myTypes.Where{
+		"deleted_at": map[string]string{"is": "null"},		
+	})
+
+	return nil
 }

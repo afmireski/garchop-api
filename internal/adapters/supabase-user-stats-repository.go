15db3bfd9a2bf314569427472d2149a338107248
afmireski <afmireski@outlook.com>
2024-06-03 21:45:06 -0300

    #50 - feat: Implement 'Create' logic in SupabaseUserStatsRepository

---
+++ internal/adapters/supabase-user-stats-repository.go
@@ -0,0 +1,39 @@
+package adapters
+
+import (
+	"github.com/afmireski/garchop-api/internal/models"
+	myTypes "github.com/afmireski/garchop-api/internal/types"
+	"github.com/nedpals/supabase-go"
+)
+
+type SupabaseUserStatsRepository struct {
+	client *supabase.Client
+}
+
+func NewSupabaseUserStatsRepository(client *supabase.Client) *SupabaseUserStatsRepository {
+	return &SupabaseUserStatsRepository{
+		client: client,
+	}
+}
+
+func (s *SupabaseUserStatsRepository) Create(input myTypes.CreateUserStatsInput) (string, error) {
+	var supabaseData []myTypes.AnyMap
+
+	err := s.client.DB.From("user_stats").Insert(input).Execute(&supabaseData); if err != nil {
+		return "", err
+	}
+
+	return supabaseData[0]["id"].(string), nil
+}
+
+func (s *SupabaseUserStatsRepository) FindById(id string, where myTypes.Where) (*models.UserStatsModel, error) {
+	panic("implement me")
+}
+
+func (s *SupabaseUserStatsRepository) Update(id string, input myTypes.AnyMap, where myTypes.Where) (*models.UserStatsModel, error) {
+	panic("implement me")
+}
+
+func (s *SupabaseUserStatsRepository) Delete(id string, where myTypes.Where) error {
+	panic("implement me")
+}

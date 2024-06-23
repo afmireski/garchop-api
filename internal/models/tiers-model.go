2a6dda4f2ed22bc218968fe65a1561cb607a4ec8
afmireski <afmireski@outlook.com>
2024-06-23 15:18:24 -0300

    #51 - refactor: Add Tier infos to UserStatsModel and PreviousTier info to TiersModel

--- internal/models/tiers-model.go
+++ internal/models/tiers-model.go
@@ -11,4 +11,5 @@ type TierModel struct {
 	CreatedAt         time.Time  `json:"created_at"`
 	UpdatedAt         time.Time  `json:"updated_at"`
 	DeletedAt         *time.Time `json:"deleted_at,omitempty"`
+	PreviousTier      *TierModel `json:"tiers,omitempty"`
 }

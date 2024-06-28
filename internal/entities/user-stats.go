2a149ca5f4cecbe84ac3b85eb7994dd9a8b54b31
afmireski <afmireski@outlook.com>
2024-06-27 21:19:57 -0300

    #56 - feat: Define UserStats entity

---
+++ internal/entities/user-stats.go
@@ -0,0 +1,19 @@
+package entities
+
+import "github.com/afmireski/garchop-api/internal/models"
+
+type UserStats struct {
+	UserId     string `json:"user_id"`
+	TierId     uint   `json:"tier_id"`
+	Experience uint   `json:"experience"`
+	Tier       *Tier  `json:"tiers"`
+}
+
+func BuildUserStatsFromModel(model models.UserStatsModel) *UserStats {
+	return &UserStats{
+		UserId:     model.UserId,
+		TierId:     model.TierId,
+		Experience: model.Experience,
+		Tier:       BuildTierFromModel(*model.Tier),
+	}
+}

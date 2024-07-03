a02955c9e15b117799c09c77d9f761b4dc49ed95
afmireski <afmireski@outlook.com>
2024-07-03 20:41:07 -0300

    #58 - refactor: Pass PrizeTypeEnum to enums package

--- internal/models/rewards-model.go
+++ internal/models/rewards-model.go
@@ -2,23 +2,20 @@ package models
 
 import (
 	"time"
-)
 
-type PrizeTypeEnum string
-
-const (
-	Pokemon PrizeTypeEnum = "pokemon"
+	"github.com/afmireski/garchop-api/internal/types/enums"
 )
 
 type RewardModel struct {
-	Id                 string        `json:"id"`
-	TierId             uint          `json:"tier_id"`
-	Name               string        `json:"name"`
-	Description        *string       `json:"description"`
-	ExperienceRequired uint          `json:"experience_required"`
-	Prize              string        `json:"prize"`
-	PrizeType          PrizeTypeEnum `json:"prize_type"`
-	CreatedAt          time.Time     `json:"created_at"`
-	UpdatedAt          time.Time     `json:"updated_at"`
-	DeletedAt          *time.Time    `json:"deleted_at,omitempty"`
+	Id                 string              `json:"id"`
+	TierId             uint                `json:"tier_id"`
+	Name               string              `json:"name"`
+	Description        *string             `json:"description"`
+	ExperienceRequired uint                `json:"experience_required"`
+	Prize              string              `json:"prize"`
+	PrizeType          enums.PrizeTypeEnum `json:"prize_type"`
+	CreatedAt          time.Time           `json:"created_at"`
+	UpdatedAt          time.Time           `json:"updated_at"`
+	DeletedAt          *time.Time          `json:"deleted_at,omitempty"`
+	Tier               *TierModel          `json:"tiers"`
 }

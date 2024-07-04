303ed192d206ba0961b20d6f0d4841f1b9d2b15f
afmireski <afmireski@outlook.com>
2024-07-03 21:14:50 -0300

    #58 - feat: Implement SupabaseRewardsRepository
    - Define SupabaseRewardsRepository
    - Implement FindAll method

--- internal/ports/rewards-repository.port.go
+++ internal/ports/rewards-repository.port.go
@@ -1,12 +1,12 @@
 package ports
 
 import (
-	"github.com/afmireski/garchop-api/internal/entities"
+	"github.com/afmireski/garchop-api/internal/models"
 	myTypes "github.com/afmireski/garchop-api/internal/types"
 )
 
 type RewardsRepositoryPort interface {
 	Create(input myTypes.Any) (string, error)	
-	FindAll(where myTypes.Where) ([]entities.Reward, error)
+	FindAll(where myTypes.Where) ([]models.RewardModel, error)
 	Delete(id string, where myTypes.Where) error
 }

0fc0e9df82d556babc31836c82d9ee6569081502
afmireski <afmireski@outlook.com>
2024-07-03 21:29:40 -0300

    #58 - feat: Implement '/rewards' route

---
+++ internal/web/routers/rewards-router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"github.com/afmireski/garchop-api/internal/web/controllers"
+	"github.com/go-chi/chi/v5"
+)
+
+func SetupRewardsRouter(router *chi.Mux, controller *controllers.RewardsController) {
+	router.Get("/rewards", controller.ListAllRewards)	
+}

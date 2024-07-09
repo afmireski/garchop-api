aab5f6aa7364d72ee8efef58fd715beacc9dc81e
afmireski <afmireski@outlook.com>
2024-07-08 21:44:18 -0300

    #61 - feat: Define UserRoleMiddleware

---
+++ internal/web/middlewares/user-role-middleware.go
@@ -0,0 +1,29 @@
+package middlewares
+
+import (
+	"encoding/json"
+	"net/http"
+
+	customErrors "github.com/afmireski/garchop-api/internal/errors"
+	"github.com/afmireski/garchop-api/internal/types/enums"
+)
+
+func UserRoleMiddleware(expectedRole enums.UserRoleEnum) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			headers := r.Header
+			role := headers.Get("UserRole")
+	
+			if role != string(expectedRole) {
+				w.Header().Set("Content-Type", "application/json")
+				err := customErrors.NewInternalError("You cannot have necessary roles for access this resources", 403, []string{})
+				w.WriteHeader(err.HttpCode)
+				json.NewEncoder(w).Encode(err)
+				return
+			}
+	
+			next.ServeHTTP(w, r)
+		})
+	}
+}

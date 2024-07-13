90048626e39eb8a1178faa8ac1085697a6b8bf39
Alexandre Tolomeotti <alexandretolomeotti@alunos.utfpr.edu.br>
2024-07-13 11:44:36 -0300

    #57 - feat: Add type ``CreateRewardInput`` on rewards repo. port

--- internal/ports/rewards-repository.port.go
+++ internal/ports/rewards-repository.port.go
@@ -1,12 +1,24 @@
 package ports
 
 import (
+	"encoding/json"
+
 	"github.com/afmireski/garchop-api/internal/models"
 	myTypes "github.com/afmireski/garchop-api/internal/types"
+	"github.com/afmireski/garchop-api/internal/types/enums"
 )
 
 type RewardsRepositoryPort interface {
-	Create(input myTypes.Any) (string, error)	
+	Create(input CreateRewardInput) (string, error)
 	FindAll(where myTypes.Where) ([]models.RewardModel, error)
 	Delete(id string, where myTypes.Where) error
+}
+
+type CreateRewardInput struct {
+	TierId             uint                `json:"tier_id"`
+	Name               string              `json:"name"`
+	Description        string              `json:"description"`
+	ExperienceRequired uint                `json:"experience_required"`
+	Type               enums.PrizeTypeEnum `json:"prize_type"`
+	Prize              json.RawMessage     `json:"prize"`
 }

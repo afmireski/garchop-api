9055354aedd5ebbb346b9263a4107a6246ba64e2
Alexandre Tolomeotti <alexandretolomeotti@alunos.utfpr.edu.br>
2024-07-13 11:41:09 -0300

    #57 - feat: Implement ``NewReward()`` on rewards service

--- internal/services/rewards-service.go
+++ internal/services/rewards-service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"github.com/afmireski/garchop-api/internal/entities"
 	"github.com/afmireski/garchop-api/internal/ports"
+	"github.com/afmireski/garchop-api/internal/validators"
 
 	customErrors "github.com/afmireski/garchop-api/internal/errors"
 	myTypes "github.com/afmireski/garchop-api/internal/types"
@@ -16,6 +17,37 @@ func NewRewardsService(rewardsRepository ports.RewardsRepositoryPort) *RewardsSe
 	return &RewardsService{
 		rewardsRepository: rewardsRepository,
 	}
+}
+
+func validateNewRewardInput(input myTypes.NewRewardInput) *customErrors.InternalError {
+	if !validators.IsValidNumericId(input.TierId) {
+		return customErrors.NewInternalError("invalid tier id", 400, []string{})
+	}
+
+	return nil
+}
+
+func (s *RewardsService) NewReward(input myTypes.NewRewardInput) *customErrors.InternalError {
+	if inputErr := validateNewRewardInput(input); inputErr != nil {
+		return inputErr
+	}
+
+	data := ports.CreateRewardInput{
+		TierId:             input.TierId,
+		Name:               input.Name,
+		Description:        input.Description,
+		ExperienceRequired: input.ExperienceRequired,
+		Type:               input.Type,
+		Prize:              input.Prize,
+	}
+
+	_, err := s.rewardsRepository.Create(data)
+
+	if err != nil {
+		return customErrors.NewInternalError("a failure occurred when trying to create a reward", 500, []string{})
+	}
+
+	return nil
 }
 
 func (r *RewardsService) ListAllRewards() ([]entities.Reward, *customErrors.InternalError) {

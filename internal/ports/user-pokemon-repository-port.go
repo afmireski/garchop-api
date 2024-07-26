dfab2175df796a08b7ed6b0cfe517b11dbe21a43
afmireski <afmireski@outlook.com>
2024-07-26 14:14:14 -0300

    #61 - feat: Define UserPokemonRepositoryPort

---
+++ internal/ports/user-pokemon-repository-port.go
@@ -0,0 +1,13 @@
+package ports
+
+import (
+	"github.com/afmireski/garchop-api/internal/models"
+	myTypes "github.com/afmireski/garchop-api/internal/types"
+)
+
+type UserPokemonRepositoryPort interface {
+	Create(input myTypes.UserPokemonId) *models.UserPokemonModel
+	FindById(id string, where myTypes.Where) (*models.UserPokemonModel, error)
+	FindAll(where myTypes.Where) ([]models.UserPokemonModel, error)
+	Update(id myTypes.UserPokemonId, where myTypes.Where) error
+}

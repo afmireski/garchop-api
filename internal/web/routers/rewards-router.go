b9ff2f77c876de8a2204d00ff7f04b477e33dd49
Alexandre <alexandretolomeotti@alunos.utfpr.edu.br>
2024-07-26 15:29:07 -0300

    #59 - feat: Add route to list claimed rewards by user

--- internal/web/routers/rewards-router.go
+++ internal/web/routers/rewards-router.go
@@ -2,9 +2,13 @@ package routers
 
 import (
 	"github.com/afmireski/garchop-api/internal/web/controllers"
+	"github.com/afmireski/garchop-api/internal/web/middlewares"
 	"github.com/go-chi/chi/v5"
+	"github.com/nedpals/supabase-go"
 )
 
-func SetupRewardsRouter(router *chi.Mux, controller *controllers.RewardsController) {
-	router.Get("/rewards", controller.ListAllRewards)	
+func SetupRewardsRouter(router *chi.Mux, controller *controllers.RewardsController, supabaseClient *supabase.Client) {
+	router.Get("/rewards", controller.ListAllRewards)
+	router.With(middlewares.SupabaseAuthMiddleware(supabaseClient),
+		middlewares.UserRoleMiddleware("client")).Get("/rewards", controller.ListRewardsByUser)
 }

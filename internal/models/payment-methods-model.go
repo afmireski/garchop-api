fdfdc05eeab3e2087aed801d7c6ff3f763d4027f
afmireski <afmireski@outlook.com>
2024-08-02 21:42:46 -0300

    #61 - feat: Define PaymentMethod model

---
+++ internal/models/payment-methods-model.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type PaymentMethodModel struct {
+	Id        string     `json:"id"`
+	Name      string     `json:"name"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty"`
+}
